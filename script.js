98b3f2bbbd4939d1ab1a90e63e75ab7f084e1659
BB-wq88 <BenBelferder@gmail.com>
2026-07-02 13:29:09 -0400

    Last Day's Work

---
+++ script.js
@@ -0,0 +1,9 @@
+// Body button
+document.getElementById("bodyBtn").addEventListener("click", function () {
+    window.open("https://www.google.com", "_blank");
+});
+
+// Hair button
+document.getElementById("hairBtn").addEventListener("click", function () {
+    window.open("https://www.youtube.com", "_blank");
+});
